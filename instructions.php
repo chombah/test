e84b0a48c0003a5778167e0c4f1aa9bc97338109
chombah <chombakeith@gmail.com>
2022-06-21 09:29:18 +0300

    initial

---
+++ instructions.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <style>
+        body {
+            background-color: #efb778;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>instructions</title>
+</head>
+<body>
+<p>In this tutorial, the exam is placed on Exams link located on Course Menu.</p>
+<p>1. On the Course Menu, click on Exams link. You will see a list of exams.
+</p>
+<p>2. When an exam is available for you to take, the exam link will be visible to students. Under the exam Title, you will see the detailed information about the exam such as how long it takes to finish and when it is available. The Begin button is for you to start the exam.</p>
+<p>3. When you are ready to take the exam, click on the Begin button. In a timed exam, the clock starts running as soon as you click Begin, even if you log out of Blackboard or turn off your computer. So, after you have clicked on the Begin button, you need to complete your exam within its time limit or before it is due time, whichever comes first.
+</p>
+<p>    4. When taking an exam, click Save Answer for EVERY question you've answered. To verify the answers have been saved, click on Test/Survey Status link at the top of the exam page. The question number with an answer saved will be grayed out. When you change an answer, you will need to click Save Answer again to update your new answer. Be sure to click on Save Answer or make sure the a "Save" check mark shown for EVERY question you have answered. Do not use Save All Answers button since there have been reported problems with Save All Answers button not working.
+</p>
+<p>    5. After you have answered all the questions, click Save and Submit . You will be prompted to confirm the submission of the exam. If you are all set, click OK to confirm.
+</p>
+<p>    6. You may click on OK (bottom right) to review the test result, your grade with the answers you have saved. Most professors do not release correct answers until all students have completed an exam.
+</p>
+<p>    If you experience technical difficulty during taking an exam (e.g. the answers are not saved, you lost Internet connection momentarily, or the exam window is mistakenly closed), you may close the web browser (or restart your computer) and log back on to your course and repeat the above steps to take the exam again.
+</p>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <style>
+        body {
+            background-color: #faf9f9;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Medical Test</title>
+</head>
+<body>
+<p>1. What is the most common cause of massive rectal bleeding in an adult?</p>
+<p> (A) carcinoma of the left colon</p>
+<p>(B) carcinoma of the right colon</p>
+<p> (C) diverticulosis</p>
+<p> (D) mesenteric thrombosis</p>
+<p>(E) polyps</p>
+<p>2. How many bones are fractured in the hand x-ray shown?</p>
+<p>(A) one</p>
+<p>(B) two<p>
+<p>(C) three</p>
+<p>(D) four</p>
+<p>(E) five</p>
+<img src="image/xray.jpg" alt="">
+<p>3. A 45-year-old man presents after being found on the sidewalk. He smells of alcohol and admits to chronic use. His vital signs are BP, 90/60; P, 125; R, 24; and T, 38.0º C (101.5º F). An ultrasound of his abdomen is shown. A CBC, chest X-ray, and blood culture have been ordered. What test is most likely to be diagnostic?</p>
+<p>(A) arterial blood gas</p>
+<p>(B) CT scan of the abdomen</p>
+<p>(C) paracentesis</p>
+<p> (D) serum transaminase levels</p>
+<iframe width="333" height="250" src="https://www.youtube.com/embed/Lz019LXDPYU" title="Question 2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</body>
+</html>
